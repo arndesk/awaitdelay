f63c2243ffdfcdeb8285b651cbc289995806c669
arndesk <140757328+arndesk@users.noreply.github.com>
2023-07-28 14:11:59 +0600

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 export declare function awaitdelay(promise: Promise<any>, options: { timeout: number }): Promise<any>;
 
-export declare class PromiseUtilsError extends Error {
+export declare class UtilsError extends Error {
+    constructor(message?: string);
+}
+
+export declare class TimeoutError extends Error {
     constructor(message?: string);
 }
